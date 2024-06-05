8e51b48787dfca39a25848aaaf679715db283ee2
jamsonchan <jamsonchan@potix.com>
2024-06-05 14:16:43 +0800

    init

--- src/main/java/org/1.ts
+++ src/main/java/org/1.ts
@@ -1,2 +1,12 @@
-const rand = crypto.getRandomValues(new Uint32Array(1))[0] / 4294967296;
+duration(): number {
+    let ms = this.ms * Math.pow(this.factor, this.attempts++);
+    if (this.jitter) {
+        // eslint-disable-next-line zk/noMixedHtml
+        crypto.randomBytes(3);
+        const rand = (crypto.getRandomValues(new Uint32Array(1))[0] / 4294967296), // 4294967296 == 2^32
+            deviation = Math.floor(rand * this.jitter * ms);
+        ms = (Math.floor(rand * 10) & 1) === 0 ? ms - deviation : ms + deviation;
+    }
+    return Math.min(ms, this.max) | 0;
+}
 const r = Math.random(3);
